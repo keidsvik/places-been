db8647ae495edf66e468a538324cb813408738d5
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-06 15:01:50 -0700

    refactor our js and make some adjustments to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,24 @@
 // Business Logic Stuffs
-function Location(location, landmarks, timeOfYear, notes) {
-  this.location = location;
-  this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
+function Location(info) {
+  // this.location = location;
+  // this.landmarks = landmarks;
+  // this.timeOfYear = timeOfYear;
+  // this.notes = notes;
+  this.info = info;
 }
+var myNewLocation = new Location([["City: New York"], ["Places to visit: Lake George", "Art Institute of History & Art"], ["Season to visit:", "Autumn"], ["Upstate New York is rural and the trees turn to beautiful colors in the fall"]]);
+var myNewLocation2 = new Location("Chicago", ["The Bean", "Navy Pier", "Lincoln Park Zoo"], "Not winter", "Chicago, officially the City of Chicago, is the most populous city in the U.S. state of Illinois, and the third-most-populous city in the United States.");
+var myNewLocation3 = new Location("Black Rock Desert", ["Playa", "lava beds"], "Summer", "Go to Burning Man, I guess.");
 
-var newYork = {
-  location: "New York",
-  landmarks: ["Lake George", "Art Institute of History & Art"],
-  timeOfYear: "Autumn",
-  notes: "Upstate New York is rural and the trees bloom with beautiful colors", 
-
-};
-
-var chicago  = {
-  location: "Chicago",
-  landmarks: ["The Bean", "Navy Pier", "Lincoln Park Zoo"],
-  timeOfYear: "Not winter", 
-  notes: "Chicago, officially the City of Chicago, is the most populous city in the U.S. state of Illinois, and the third-most-populous city in the United States. ", 
-
-};
-
-var blackRockDesert  = {
-  location: "Black Rock Desert",
-  landmarks: ["Playa", "lava beds"],
-  timeOfYear: "Summer", 
-  notes: "Go to Burning Man, I guess.", 
-
-};
 // User Interface Logic Stuffs
 
 $(document).ready(function() {
-  event.preventDefault();
- $("#resultsDiv").text("");
- var newYork = ($("nyResults").val());
- var chicago = ($("#chicagoResults").val());
- var blackRockDesert = ($("#blackRockDesertResults").val());
+  // event.preventDefault();
+ $("h3").click(function(){
+   $(this).html(myNewLocation.info.join(" "));
 
-
+});
+  
 })
+
+
