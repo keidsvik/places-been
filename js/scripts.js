089f2b27c6b641b4b710bf9d393b939cb789b320
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-06 11:44:31 -0700

    add in initial Javascript

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function Location(location, landmarks, timeOfYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+var newYork = {
+  location: "New York",
+  landmarks: "Lake George", "Art Institute of History & Art",
+  timeOfYear: "Autumn",
+  notes: "Upstate New York is rural and the trees bloom with beautiful colors", 
+
+};
+
+var chicago  = {
+  location: "Chicago",
+  landmarks: "The Bean", "Navy Pier", "Lincoln Park Zoo",
+  timeOfYear: "Not winter", 
+  notes: "Chicago, officially the City of Chicago, is the most populous city in the U.S. state of Illinois, and the third-most-populous city in the United States. ", 
+
+};
+
+var blackRockDesert  = {
+  location: "Black Rock Desert",
+  landmarks: "Playa", "lava beds",
+  timeOfYear: "Not winter", 
+  notes: "Chicago, officially the City of Chicago, is the most populous city in the U.S. state of Illinois, and the third-most-populous city in the United States. ", 
+
+};
